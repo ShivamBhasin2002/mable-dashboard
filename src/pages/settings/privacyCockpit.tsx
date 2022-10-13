13935e75acd90dfa73c73770047a811f1b4edc14
Kallyan99 <kallyan@99ideassaas.com>
2022-10-13 22:16:04 +0530

    hight width value changes on tailwind.config and added parameter settings on  privacyCockpit

--- src/pages/settings/privacyCockpit.tsx
+++ src/pages/settings/privacyCockpit.tsx
@@ -1,7 +1,55 @@
+import { ComponentWrapper } from 'components/common';
 import React from 'react';
-
+const data_collection_destinations: {
+  value: string;
+  label: string;
+  available: boolean;
+}[] = [
+  {
+    value: 'database',
+    label: 'Database',
+    available: true
+  },
+  {
+    value: 'facebook',
+    label: 'Facebook',
+    available: true
+  },
+  {
+    value: 'tiktok',
+    label: 'Tik Tok',
+    available: false
+  }
+];
 function privacyCockpit() {
-  return <div>privacyCockpit</div>;
+  return (
+    <>
+      <ComponentWrapper className="flex flex-col">
+        <div className="active_all flex justify-end">
+          <p className="text-light">Active Everything</p>
+        </div>
+        <div className="header w-full flex items-center">
+          <div className="header_title  text-light w-30 text-[2em] font-[700]">
+            Parameter Settings
+          </div>
+          <div className="options w-70 flex justify-evenly ">
+            {data_collection_destinations.map((item) => {
+              return (
+                <p
+                  key={item.value}
+                  className={
+                    item.available ? `text-gray-300 text-[1.2em]` : `text-gray-500 text-[1.2em]`
+                  }
+                >
+                  {item.label}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+      </ComponentWrapper>
+    </>
+  );
 }
 
 export default privacyCockpit;
