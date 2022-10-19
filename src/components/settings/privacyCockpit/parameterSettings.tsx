7e122dc5a8a9167e7f4170da17d6fb73305e034e
Kallyan01 <singhakallya@gmail.com>
2022-10-19 15:35:35 +0530

    added style changes

--- src/components/settings/privacyCockpit/parameterSettings.tsx
+++ src/components/settings/privacyCockpit/parameterSettings.tsx
@@ -1,6 +1,6 @@
 import ComponentWrapper from 'components/common/ComponentWrapper';
 import ToggleBtn from 'components/common/ToggleBtn/Togglebtn';
-import { Divider } from '@chakra-ui/react';
+import { Button, Divider } from '@chakra-ui/react';
 import ToggleTable from './toggle_table';
 function parameterSettings() {
   const data_collection_destinations: {
@@ -26,12 +26,8 @@ function parameterSettings() {
   ];
   return (
     <ComponentWrapper className="flex flex-col">
-      <div className="active_all flex justify-end items-center">
-        <p className="text-light mx-2">Active Everything </p>
-        <ToggleBtn value={false} on={'on'} off={'off'} />
-      </div>
       <div className="header w-full grid grid-cols-12 gap-0  content-center">
-        <div className="header_title text-[2em] font-[700] text-primary col-span-3  ">
+        <div className="header_title text-[1.8em] font-[700] text-primary col-span-3  ">
           Parameter Settings
         </div>
 
@@ -53,6 +49,18 @@ function parameterSettings() {
       <Divider className="my-3" />
       <div className="toggleArea">
         <ToggleTable />
+      </div>
+      <Divider className="my-3" />
+      <div className="flex justify-between items-center">
+        <div className="active_all flex justify-end items-center">
+          <p className="text-light mx-2 opacity-50">Active Everything </p>
+          <ToggleBtn value={false} on={'on'} off={'off'} />
+        </div>
+        <div className="button">
+          <Button className="w-[8rem] mt-5" type="submit" colorScheme="blue">
+            Save
+          </Button>
+        </div>
       </div>
     </ComponentWrapper>
   );
