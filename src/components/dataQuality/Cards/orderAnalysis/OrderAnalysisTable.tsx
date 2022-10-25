a38a6ae9a1d28f890e423b8f0881a634d13e1ca6
Shivam Bhasin <bhasinshivam2002@gmail.com>
2022-10-26 02:52:46 +0530

    feat: added soprting to order analysis tabloe

--- src/components/dataQuality/Cards/orderAnalysis/OrderAnalysisTable.tsx
+++ src/components/dataQuality/Cards/orderAnalysis/OrderAnalysisTable.tsx
@@ -4,7 +4,7 @@ import Icon from 'assets/icons';
 import { ComponentWrapper } from 'components/common';
 import StatusSelectorMenu from 'components/dataQuality/General/StatusSelecterMenu';
 
-import { statusSelector, STATUS_TYPE } from 'utility/constants/enums';
+import { SORT_ORDER, statusSelector, STATUS_TYPE } from 'utility/constants/enums';
 import { noOrdersMessage } from 'utility/constants/strings';
 
 import { useSelector, useDispatch } from 'redux/store';
@@ -12,14 +12,17 @@ import { orderAnalysisAsync } from 'redux/reducers/dataQuality/orderAnalysisSlic
 import { useWindowSize } from 'utility/customHooks';
 import OrderDetails from './OrderDetails';
 import { order } from 'utility/typeDefinitions/reduxTypes';
+import moment from 'moment';
 
 const OrderAnalysisTable = () => {
-  const dispatch = useDispatch();
   const { tableData, status, statusSelected } = useSelector((state) => state.orderAnalysis);
   const [page, setPage] = useState(1);
   const [orders, setOrders] = useState<order[]>([]);
+  const [sortOrder, setSortOrder] = useState<SORT_ORDER>(SORT_ORDER.INCREASING);
   const refresh = useSelector((state) => state.dates.refresh);
   const { width: screenWidth } = useWindowSize();
+
+  const dispatch = useDispatch();
   useEffect(() => {
     if (status !== STATUS_TYPE.FETCHING) dispatch(orderAnalysisAsync());
   }, [refresh]);
@@ -28,11 +31,26 @@ const OrderAnalysisTable = () => {
   }, [statusSelected]);
   useEffect(() => {
     setOrders(
-      tableData.filter(
-        ({ status }) => statusSelected === statusSelector.all || status === statusSelected
-      )
+      tableData
+        .filter(({ status }) => statusSelected === statusSelector.all || status === statusSelected)
+        .sort((order1, order2) =>
+          sortOrder === SORT_ORDER.INCREASING
+            ? moment(order1.created_at).diff(moment(order2.created_at))
+            : -1 * moment(order1.created_at).diff(moment(order2.created_at))
+        )
     );
   });
+
+  const changeSortOrder = () => {
+    switch (sortOrder) {
+      case SORT_ORDER.INCREASING:
+        setSortOrder(SORT_ORDER.DECREASING);
+        break;
+      case SORT_ORDER.DECREASING:
+        setSortOrder(SORT_ORDER.INCREASING);
+        break;
+    }
+  };
 
   return (
     <ComponentWrapper
@@ -57,7 +75,9 @@ const OrderAnalysisTable = () => {
             </tr>
             <tr className="[&>*]:font-montserrat [&>*]:text-[14px] [&>*]:font-extrabold [&>*]:py-[12px] [&>*]:px-[20px] [&>*]2xl:whitespace-nowrap flex [&>*]:flex-1">
               <td className="bg-primary rounded-tl-[10px]">Order</td>
-              <td className="bg-primary">Date</td>
+              <td className="bg-primary flex gap-6 items-center" onClick={changeSortOrder}>
+                Date <Icon icon={sortOrder} className="cursor-pointer" />
+              </td>
               <td className="bg-primary">Customer</td>
               <td className="bg-primary">Total</td>
               <td className="bg-primary/[0.8]">CV</td>
