5b28bfddd7875a43521e302de224f4b1f45ab3f2
flying-solo <flyingsoloharshit@gmail.com>
2022-11-02 00:33:36 +0530

    event usage estimated

--- src/components/settings/accountSettings/eventUsage.tsx
+++ src/components/settings/accountSettings/eventUsage.tsx
@@ -8,10 +8,20 @@ import { useDispatch, useSelector } from 'redux/store';
 import { TierRange } from 'redux/reducers/eventUsageSlice';
 
 const EventUsage = () => {
+  function daysInThisMonth() {
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+  }
+
   const [selectedTier, setTier] = useState('Tier 1');
   const dispatch = useDispatch();
   const { range } = useSelector((state) => state.eventUsage.selectedTier);
+  const todayDate = new Date();
   const { current_month: currentMonth } = useSelector((state) => state.eventUsage.monthEvents);
+
+  const estimatedEvent =
+    (currentMonth / todayDate.getDate()) * daysInThisMonth() - todayDate.getDate();
+
   const dropdown = () => (
     <Menu gutter={0} isLazy>
       <MenuButton
@@ -64,15 +74,45 @@ const EventUsage = () => {
     >
       <div className="flex justify-end mb-6 mt-6">
         <div className="flex flex-col items-end">
-          <div className="">Estimated Tier this month:</div>
-          <div className=" text-xl text-light font-bold">Tier 2 - 149E</div>
+          <div className="">Estimated tier this month</div>
+          <div className=" text-xl text-light font-bold">
+            {selectedTier} - {estimatedEvent}
+          </div>
         </div>
       </div>
-      <div className="h-[1.2rem] w-full bg-secondary/[0.3] rounded-full mb-6">
-        <div
-          className={`h-full bg-primary transition-[width] ease-in-out duration-1000 rounded-full`}
-          style={{ width: `${(currentMonth / range) * 100}%` }}
-        ></div>
+
+      <div className="h-[1.2rem] w-full bg-secondary/[0.3] rounded-full mb-6 relative">
+        {currentMonth > estimatedEvent ? (
+          <>
+            <div
+              className={`h-full bg-primary transition-[width] ease-in-out duration-1000 rounded-full`}
+              style={{
+                width: `${(currentMonth / range) * 100}%`
+              }}
+            ></div>
+            <div
+              className={`h-full bg-green-800 transition-[width] ease-in-out duration-1000 rounded-full absolute top-0`}
+              style={{
+                width: `${(estimatedEvent / range) * 100}%`
+              }}
+            ></div>
+          </>
+        ) : (
+          <>
+            <div
+              className={`h-full bg-green-800 transition-[width] ease-in-out duration-1000 rounded-full`}
+              style={{
+                width: `${(estimatedEvent / range) * 100}%`
+              }}
+            ></div>
+            <div
+              className={`h-full bg-primary transition-[width] ease-in-out duration-1000 rounded-full absolute top-0`}
+              style={{
+                width: `${(currentMonth / range) * 100}%`
+              }}
+            ></div>
+          </>
+        )}
       </div>
 
       <div className=" flex justify-between">
@@ -88,11 +128,11 @@ const EventUsage = () => {
 
         <div className="flex flex-col items-end">
           <div className="flex mr-10 text-gray-400">
-            <div className="w-5 h-5 bg-primary rounded-full mr-2"></div>
-            Estimated tier this month
+            <div className="w-5 h-5 bg-green-800 rounded-full mr-2"></div>
+            Estimated event this month
           </div>
           <div className="text-lg">
-            {currentMonth}/{range}
+            {(currentMonth / todayDate.getDate()) * daysInThisMonth() - todayDate.getDate()}/{range}
           </div>
         </div>
       </div>
