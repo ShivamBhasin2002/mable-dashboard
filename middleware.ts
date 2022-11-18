85516616f5d68076a36d4ea0ad56ccdb3f2e69b5
flying-solo <flyingsoloharshit@gmail.com>
2022-11-18 14:12:06 +0530

    redirect issue middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { routes } from '@utility/constants/enums';
 import { NextResponse, NextRequest } from 'next/server';
 
 export const middleware = async (req: NextRequest) => {
-  const { pathname } = req.nextUrl;
-  if (!Object.values(routes).includes(pathname as routes)) NextResponse.redirect(routes.dashboard);
+  const { pathname, origin } = req.nextUrl;
+  if (!Object.values(routes).includes(pathname as routes)) NextResponse.redirect(`${origin}/${routes.dashboard}`);
   NextResponse.next();
 };
