52b555e952cb1ad0bc7d482cbba411038fcd22ca
flying-solo <flyingsoloharshit@gmail.com>
2022-11-21 15:18:55 +0530

    middleware

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { routes } from '@utility/constants/enums';
-import { NextResponse, NextRequest } from 'next/server';
-
-export const middleware = async (req: NextRequest) => {
-  const { pathname, origin } = req.nextUrl;
-  if (!Object.values(routes).includes(pathname as routes)) NextResponse.redirect(`${origin}/${routes.dashboard}`);
-  NextResponse.next();
-};
